4d9a2e9210380d7c4c95157a2fbde7e19d171a1b
Nihal00 <nihallsuvarna@gmail.com>
2023-07-24 19:09:51 +0530

    submitting code-Mon Jul 24 2023 19:09:50 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [isTrue, setIsTrue] = useState(false);
+
+  function check(){
+    if(name.trim() !== "" && password !== "") {
+      setIsTrue(true);
+    } else {
+      setIsTrue(false);
+    }
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <form>
+        <label>
+          UserName:
+          <input
+            type="text"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        {isTrue ? "" : <p style={{color: "red"}}>Both username and password are required</p>}
+        <button type="submit" onClick={check}>Login</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
